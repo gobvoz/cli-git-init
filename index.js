99849889fa8117dd5e5a448c780c9875c894f845
Yevgeniy Starinets <tsn.ipad@gmail.com>
2023-07-05 12:02:48 -0400

    - remove unused dependencies; - add `log` util.

--- index.js
+++ index.js
@@ -4,29 +4,14 @@ import path from 'path';
 import url from 'url';
 import { exec } from 'child_process';
 import dotenv from 'dotenv';
-import chalk from 'chalk';
 import clear from 'clear';
 import figlet from 'figlet';
 import { Octokit } from 'octokit';
 
+import log from './utils/log.js';
+
 const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
-const log = {
-  _log: (message, prefix) => console.log(`%s ${message}`, prefix),
-  done: function (message) {
-    this._log(message, '\x1b[32m\x1b[01mDONE \x1b[0m');
-  },
-  error: function (message) {
-    this._log(message, '\x1b[31m\x1b[01mERROR \x1b[0m');
-  },
-  warning: function (message) {
-    this._log(message, '\x1b[33m\x1b[01mWARNING \x1b[0m');
-  },
-  yellow: function (message) {
-    this._log(`\x1b[33m${message}\x1b[0m`);
-  },
-};
 
 const envPath = path.resolve(__dirname, '.env');
 if (!fs.existsSync(envPath)) {
@@ -49,14 +34,14 @@ if (fs.existsSync('.git')) {
   await new Promise((resolve, reject) =>
     exec('git init', (error, stdout, stderr) => {
       if (error) {
-        console.log(`%s ${error.message}`, chalk.red.bold('ERROR'));
+        log.error(error.message);
         reject(false);
       }
       if (stderr) {
-        console.log(`%s ${stderr}`, chalk.red.bold('ERROR'));
+        log.error(stderr);
         reject(false);
       }
-      console.log(`%s ${stdout.split('\n')[0]}`, chalk.green.bold('DONE'));
+      log.done(stdout.split('\n')[0]);
       resolve(true);
     }),
   );
@@ -64,19 +49,19 @@ if (fs.existsSync('.git')) {
 
 const packageJsonPath = path.resolve('package.json');
 if (fs.existsSync(packageJsonPath)) {
-  console.log(`%s ${packageJsonPath} already exists`, chalk.yellow.bold('WARNING'));
+  log.warning(`${packageJsonPath} already exists`);
 } else {
   await new Promise((resolve, reject) =>
     exec('npm init -y', (error, stdout, stderr) => {
       if (error) {
-        console.log(`%s ${error.message}`, chalk.red.bold('ERROR'));
+        log.error(error.message);
         reject(false);
       }
       if (stderr) {
-        console.log(`%s ${stderr}`, chalk.red.bold('ERROR'));
+        log.error(stderr);
         reject(false);
       }
-      console.log(`%s ${stdout.split('\n')[0]}`, chalk.green.bold('DONE'));
+      log.done(stdout.split('\n')[0]);
       resolve(true);
     }),
   );
@@ -84,30 +69,30 @@ if (fs.existsSync(packageJsonPath)) {
 
 const gitIgnorePath = path.resolve('.gitignore');
 if (fs.existsSync(gitIgnorePath)) {
-  console.log(`%s ${gitIgnorePath} already exists`, chalk.yellow.bold('WARNING'));
+  log.warning(`${gitIgnorePath} already exists`);
 } else {
   fs.writeFileSync(
     gitIgnorePath,
     '.idea\n.vscode\n.DS_Store\n\n/node_modules\n/build\n\n**/*.local\n**/*.local.*\n\n**/*.env\n**/*.env.*\n!.env.example\n\n**/*.log',
   );
-  console.log(`%s ${gitIgnorePath} created`, chalk.green.bold('DONE'));
+  log.done(`${gitIgnorePath} created`);
 }
 
 const privateKeyName = process.env.PRIVATE_KEY;
 if (!privateKeyName) {
-  console.log(`%s PRIVATE_KEY not found in .env file`, chalk.red.bold('ERROR'));
+  log.error(`PRIVATE_KEY not found in .env file`);
   process.exit();
 }
 
 const keyPath = path.resolve(__dirname, process.env.PRIVATE_KEY);
 if (!fs.existsSync(keyPath)) {
-  console.log(`%s ${keyPath} not found`, chalk.red.bold('ERROR'));
+  log.error(`${keyPath} not found`);
   process.exit();
 }
 
 const privateKey = fs.readFileSync(keyPath, 'utf8');
 if (!privateKey) {
-  console.log(`%s ${keyPath} is empty`, chalk.red.bold('ERROR'));
+  log.error(`${keyPath} is empty`);
   process.exit();
 }
 
@@ -120,13 +105,12 @@ const octokit = new Octokit({
 try {
   // Check if repo name already exists
   const result = await octokit.request('GET /user/repos', {
-    username: 'gobvoz',
     type: 'all',
   });
 
   const isNameExist = result.data.map(repo => repo.name).includes(repoName);
   if (isNameExist) {
-    console.log(`%s repository ${repoName} already exists`, chalk.yellow.bold('ERROR'));
+    log.error(`repository ${repoName} already exists`);
     process.exit();
   }
 
@@ -137,7 +121,7 @@ try {
     homepage: '',
     private: false,
   });
-  console.log(`%s ${response?.data?.html_url}`, chalk.green.bold('DONE'));
+  log.done(`${response?.data?.html_url}`);
 
   const sshUrl = response?.data?.ssh_url;
 
@@ -146,39 +130,39 @@ try {
     const addRemoteResult = await new Promise((resolve, reject) => {
       exec(`git remote add origin ${sshUrl}`, (error, stdout, stderr) => {
         if (error) {
-          console.log(`%s ${error.message}`, chalk.red.bold('ERROR'));
+          log.error(`${error.message}`);
           reject(false);
         }
         if (stderr) {
-          console.log(`%s ${stderr}`, chalk.red.bold('ERROR'));
+          log.error(stderr);
           reject(false);
         }
-        console.log(`%s 'remote' for repository added`, chalk.green.bold('DONE'));
+        log.done(`'remote' for repository added`);
         resolve(true);
       });
     });
     if (!addRemoteResult) process.exit();
   } else {
-    console.log(`%s ssh_url not found`, chalk.red.bold('ERROR'));
+    log.error(`ssh_url not found`);
   }
 
   // Initial commit
   const result1 = await new Promise((resolve, reject) =>
     exec('git add . && git commit -m "Initial commit"', (error, stdout, stderr) => {
       if (error) {
-        console.log(`%s ${error.message}`, chalk.red.bold('ERROR'));
+        log.error(error.message);
         reject(false);
       }
       if (stderr) {
-        console.log(`%s ${stderr}`, chalk.red.bold('ERROR'));
+        log.error(stderr);
         reject(false);
       }
-      console.log(`%s ${stdout.split('\n')[0]}`, chalk.green.bold('DONE'));
+      log.done(stdout.split('\n')[0]);
       resolve(true);
     }),
   );
   if (!result1) process.exit();
 } catch (error) {
-  console.log(`%s ${error.message}`, chalk.red.bold('ERROR'));
+  log.error(error.message);
   process.exit();
 }
---
+++ utils/log.js
@@ -0,0 +1,15 @@
+export default {
+  _log: (message, prefix) => console.log(`%s ${message}`, prefix),
+  done: function (message) {
+    this._log(message, '\x1b[32m\x1b[01mDONE \x1b[0m');
+  },
+  error: function (message) {
+    this._log(message, '\x1b[31m\x1b[01mERROR \x1b[0m');
+  },
+  warning: function (message) {
+    this._log(message, '\x1b[33m\x1b[01mWARNING \x1b[0m');
+  },
+  yellow: function (message) {
+    this._log(`\x1b[33m${message}\x1b[0m`);
+  },
+};
